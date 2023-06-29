6484cacbb93aa48039ae2b8def414eb1fbab87cc
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-06-29 19:12:19 +0530

    Added some more routes to the Home_Router

--- data/items.js
+++ data/items.js
@@ -1,5 +1,5 @@
-items = [
-  { id: 1687970704308, itemName: "Vada pav", price: "$1", ratings: "4/5" },
+let items = [
+  { id: 1687970704308, itemName: "Vada pav", price: "$1", ratings: "5/5" },
   {
     id: 1687970378816,
     itemName: "Chicken Paneer Biryani",
--- routes/home_router.js
+++ routes/home_router.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-const menu = require("../data/items");
+const menu = require("../data/items.js");
 const home_router = express.Router();
 
 home_router.use(bodyParser.json());
 
 // Get ROUTES
 home_router.get("/", (req, res) => {
-  res.send("I am on Home Page");
+  res.send("I am on Home Page with Menu Items");
 });
 
 home_router.get("/menu", (req, res) => {
@@ -21,6 +21,53 @@ home_router.get("/menu/get-new-items", (req, res) => {
     res.status(201).send(recentItems);
   } else {
     res.status(500).send("Server Error");
+  }
+});
+
+home_router.get("/menu/itemNames", (req, res) => {
+  const itemNames = menu.map((item) => item.itemName); // mapping thru object to get the specific parameter
+  if (itemNames) {
+    res.status(200).json(itemNames);
+  } else {
+    res.status(404).send("Item name not found");
+  }
+});
+
+home_router.get("/menu/ratings", (req, res) => {
+  const itemRatings = menu
+    .filter((item) => item.ratings) // filtering out the objects with no rating in it
+    .map((item) => {
+      // mapping thru object and then return the specify object with params
+      return {
+        id: item.id,
+        itemName: item.itemName,
+        itemRatings: item.ratings,
+      };
+    });
+  if (itemRatings.length > 0) {
+    // checking if it exists
+    res.status(200).json({ itemRatings });
+  } else {
+    res.status(404).send("No Ratings Found");
+  }
+});
+
+home_router.get("/menu/pricing", (req, res) => {
+  const itemPricing = menu
+    .filter((item) => item.price) // filtering out the objects with no price in it
+    .map((item) => {
+      // mapping thru object and then return the specify object with params
+      return {
+        id: item.id,
+        itemName: item.itemName,
+        itemPrice: item.price,
+      };
+    });
+  if (itemPricing.length > 0) {
+    // checking if it exists
+    res.status(200).json({ itemPricing }); 
+  } else {
+    res.status(404).send("No Price of items Found");
   }
 });
 
